1badf3b45a06868428d53485269b3f1e649fd464
ABUJARIRY123 <maroridhiwani@gmail.com>
2023-12-15 10:39:00 +0300

    code structure

---
+++ server/src/main/java/com/ade/duruus/DuruusApplication.java
@@ -0,0 +1,13 @@
+package com.ade.duruus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuruusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuruusApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ade/duruus/DuruusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ade.duruus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuruusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
