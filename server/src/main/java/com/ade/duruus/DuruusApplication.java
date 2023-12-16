4ec5629cf8574ae3b8f2b6ca81164078520c3cbf
ABUJARIRY123 <maroridhiwani@gmail.com>
2023-12-16 06:54:24 +0300

    comming to vs code

--- server/src/main/java/com/ade/duruus/DuruusApplication.java
+++ server/src/main/java/com/ade/duruus/DuruusApplication.java
@@ -2,12 +2,20 @@ package com.ade.duruus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class DuruusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DuruusApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Welcome to Spring";
 	}
 
 }
